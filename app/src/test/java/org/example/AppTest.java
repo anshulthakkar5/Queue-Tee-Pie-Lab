a7b4cf14a64966f7a024cae8c7bf2fa8b3a2408a
anshulthakkar5 <204286406+anshulthakkar5@users.noreply.github.com>
2026-04-25 16:31:17 -0500

    Update AppTest.java

--- app/src/test/java/org/example/AppTest.java
+++ app/src/test/java/org/example/AppTest.java
@@ -11,7 +11,7 @@ class AppTest {
         assertNotNull(app.getGreeting());
     }
 
-    // simple Cutie for testing
+    // test Cutie
     static class TestCutie implements Cutie {
         private String name;
 
@@ -29,34 +29,23 @@ class AppTest {
     }
 
     @Test
-    void testEnqueueAndSize() {
-        CutieQueue queue = new CutieQueue(2);
+    void testQueueFunctionality() {
+        App.CutieQueue queue = new App.CutieQueue(2);
 
         queue.enqueue(new TestCutie("A"));
         queue.enqueue(new TestCutie("B"));
 
         assertEquals(2, queue.size());
-    }
-
-    @Test
-    void testDequeueFIFO() {
-        CutieQueue queue = new CutieQueue(2);
-
-        TestCutie a = new TestCutie("A");
-        TestCutie b = new TestCutie("B");
-
-        queue.enqueue(a);
-        queue.enqueue(b);
 
         Cutie removed = queue.dequeue();
-
         assertEquals("A", removed.description());
+
         assertEquals(1, queue.size());
     }
 
     @Test
     void testQueueFull() {
-        CutieQueue queue = new CutieQueue(1);
+        App.CutieQueue queue = new App.CutieQueue(1);
 
         queue.enqueue(new TestCutie("A"));
         queue.enqueue(new TestCutie("B")); // should not be added
@@ -65,8 +54,8 @@ class AppTest {
     }
 
     @Test
-    void testEmptyDequeue() {
-        CutieQueue queue = new CutieQueue(2);
+    void testEmptyQueue() {
+        App.CutieQueue queue = new App.CutieQueue(1);
 
         Cutie result = queue.dequeue();
 
