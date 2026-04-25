3c4c1e885ea32d737670bb28e405321375d03e90
anshulthakkar5 <204286406+anshulthakkar5@users.noreply.github.com>
2026-04-25 16:26:21 -0500

    Update Cutie.java

--- app/src/main/java/org/example/Cutie.java
+++ app/src/main/java/org/example/Cutie.java
@@ -1,7 +1,6 @@
 package org.example;
 
 public interface Cutie {
-  public String description(); // All cuties need to have a description of what makes them cute.
-
-  public Integer cutenessRating(); // All cuties get a cuteness rating out of ten.
+    String description();
+    Integer cutenessRating();
 }
